724ff3390045916ec02d8572cba01f6ad7c652bb
Борис Шестериков <boris20033@gmail.com>
2024-02-19 15:13:35 +0400

    Make json openner and igonre letter

--- lab_1/first_task.py
+++ lab_1/first_task.py
@@ -3,9 +3,14 @@ import logging
 import os
 import re
 
+ignore_list = ['\n', '.', ',', '!', '?', ' ']
+
 def encryption(text : str, key : dict) -> str:
     resultText = ""
     for letter in text.upper():
+        if letter in ignore_list:
+            resultText += letter
+            continue
         resultText += key[letter]
     return resultText
 
@@ -14,9 +19,17 @@ def read_file(path_original : str) -> str:
         text = f.read()
     return text
 
+def json_to_dict(path : str) -> dict:
+    with open(path, 'r', encoding='utf-8') as f:
+        key = json.load(f)
+    return key
+
 if __name__ == "__main__":
-    with open('lab_1\settings.json') as f:
+    with open('lab_1\settings.json', 'r', encoding='utf-8') as f:
         setting = json.load(f)
+
+    print(encryption(read_file('lab_1\\first_task\\original.txt'), json_to_dict('lab_1\\first_task\\key.json')))
+    
     
     
     
