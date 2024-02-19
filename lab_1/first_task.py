d8340ad7172126d0c2fcd945fbbdf864a80a498b
Борис Шестериков <boris20033@gmail.com>
2024-02-19 16:19:08 +0400

    Add logging

--- lab_1/first_task.py
+++ lab_1/first_task.py
@@ -3,49 +3,63 @@ import logging
 import os
 import re
 
+logging.basicConfig(level=logging.INFO)
+
 SETTING_PATH = 'lab_1\settings.json'
 RUSSIAN = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя"
 DIGITAL = "0123456789"
 
 def encryption(text : str, key : dict) -> str:
-    # Создаем таблицу регистров для коректного выходного
-    registerTable = list()
-    for letter in text:
-        if (letter not in RUSSIAN) and (letter not in DIGITAL): continue
-        if letter.isupper():
-            registerTable.append(True)
-        else:
-            registerTable.append(False)
-        
-    # Создаем новую строчку с закодированным текстом
-    # учитывая регистор с помощью таблицы регистров
-    i = 0
-    resultText = ""
-    for letter in text.upper():
-        if (letter not in RUSSIAN) and (letter not in DIGITAL):
-            resultText += letter
-            continue
+    try:
+        # Создаем таблицу регистров для коректного выходного
+        registerTable = list()
+        for letter in text:
+            if (letter not in RUSSIAN) and (letter not in DIGITAL): continue
+            if letter.isupper():
+                registerTable.append(True)
+            else:
+                registerTable.append(False)
+            
+        # Создаем новую строчку с закодированным текстом
+        # учитывая регистор с помощью таблицы регистров
+        i = 0
+        resultText = ""
+        for letter in text.upper():
+            if (letter not in RUSSIAN) and (letter not in DIGITAL):
+                resultText += letter
+                continue
 
-        resultKey = key[letter]
-        if registerTable[i]: resultText += resultKey
-        else:                resultText += resultKey.lower()
-        i += 1
-    
-    return resultText
+            resultKey = key[letter]
+            if registerTable[i]: resultText += resultKey
+            else:                resultText += resultKey.lower()
+            i += 1
+        
+        return resultText
+    except Exception as ex:
+        logging.error(ex)
 
 def read_file(path_original : str) -> str:
-    with open(path_original, 'r', encoding='utf-8') as f:
-        text = f.read()
-    return text
+    try:
+        with open(path_original, 'r', encoding='utf-8') as f:
+            text = f.read()
+        return text
+    except FileNotFoundError as ex:
+        logging.error(ex)
 
 def json_to_dict(path : str) -> dict:
-    with open(path, 'r', encoding='utf-8') as f:
-        key = json.load(f)
-    return key
+    try:
+        with open(path, 'r', encoding='utf-8') as f:
+            key = json.load(f)
+        return key
+    except Exception as ex:
+        logging.error(ex)
 
 def write_file(path : str, text : str) -> None:
-    with open(path, 'w', encoding='utf-8') as f:
-        f.write(text)
+    try:
+        with open(path, 'w', encoding='utf-8') as f:
+            f.write(text)
+    except Exception as ex:
+        logging.error(ex)
 
 if __name__ == "__main__":
     setting = json_to_dict(SETTING_PATH)
