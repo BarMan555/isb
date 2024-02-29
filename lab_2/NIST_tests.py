e775d2a24422a2fa4918f84ad51eef7720f35915
Борис Шестериков <boris20033@gmail.com>
2024-02-29 23:13:08 +0400

    Make frequency_bit_test

--- lab_2/NIST_tests.py
+++ lab_2/NIST_tests.py
@@ -7,6 +7,13 @@ logging.basicConfig(level=logging.INFO)
 
 SEQUENCE_PATH = os.path.join('lab_2', 'sequence.json')
 
+def frequency_bit_test(sequence : str) -> float:
+    i_seq = list(map(int, sequence))
+    result_seq = [-1 if x == 0 else x for x in i_seq]
+    result_sum = sum(result_seq) / math.sqrt(len(result_seq))
+    p_value = math.erfc(result_sum / math.sqrt(2))
+    return p_value
+
 
 
 if __name__ == "__main__":
@@ -15,3 +22,5 @@ if __name__ == "__main__":
 
     cpp_seq = sequence['cpp']
     java_seq = sequence['java']
+
+    print(frequency_bit_test(cpp_seq))
