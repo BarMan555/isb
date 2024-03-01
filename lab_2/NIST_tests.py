c757fd3731feb8e0f0e5cb9b9a2ada578f317c38
Борис Шестериков <boris20033@gmail.com>
2024-03-01 15:13:08 +0400

    Make identical_consecutive_bits test

--- lab_2/NIST_tests.py
+++ lab_2/NIST_tests.py
@@ -15,6 +15,26 @@ def frequency_bit_test(sequence : str) -> float:
     return p_value
 
 
+def identical_consecutive_bits(sequence : str) -> float:
+    i_seq = list(map(int, sequence))
+
+    share_of_units = sum(i_seq) / len(i_seq) 
+    if math.fabs(share_of_units-(1/2)) >= 2/math.sqrt(len(i_seq)):
+        return 0.0
+    
+    number_of_sign_changes = 0
+    for i in range(0, len(i_seq)-1):
+        number_of_sign_changes += 0 if i_seq[i] == i_seq[i+1] else 1
+
+    p_value = math.erfc(
+        math.fabs(number_of_sign_changes-2*len(i_seq)*share_of_units*(1-share_of_units))
+        /(2*math.sqrt(2*len(i_seq))*share_of_units*(1-share_of_units))
+        )
+    
+    return p_value
+
+
+
 
 if __name__ == "__main__":
     with open(SEQUENCE_PATH, 'r', encoding='utf-8') as f:
@@ -24,3 +44,4 @@ if __name__ == "__main__":
     java_seq = sequence['java']
 
     print(frequency_bit_test(cpp_seq))
+    print(identical_consecutive_bits(cpp_seq))
