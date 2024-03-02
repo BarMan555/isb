951e236084aabcaafba4893c6aa26924ce3fa3e1
Борис Шестериков <boris20033@gmail.com>
2024-03-02 20:32:32 +0400

    Add logging

--- lab_2/NIST_tests.py
+++ lab_2/NIST_tests.py
@@ -18,11 +18,14 @@ def frequency_bit_test(sequence : str) -> float:
     
     Return: float - Probability of a random sequence
     """
-    i_seq = list(map(int, sequence))
-    result_seq = [-1 if x == 0 else x for x in i_seq]
-    result_sum = fabs(sum(result_seq)) / sqrt(len(result_seq))
-    p_value = erfc(result_sum / sqrt(2))
-    return p_value
+    try:
+        i_seq = list(map(int, sequence))
+        result_seq = [-1 if x == 0 else x for x in i_seq]
+        result_sum = fabs(sum(result_seq)) / sqrt(len(result_seq))
+        p_value = erfc(result_sum / sqrt(2))
+        return p_value
+    except Exception as ex:
+        logging.error(f"Invalid sequence : {ex}")
 
 
 def identical_consecutive_bits(sequence : str) -> float:
@@ -34,22 +37,25 @@ def identical_consecutive_bits(sequence : str) -> float:
     
     Return: float - Probability of a random sequence
     """
-    i_seq = list(map(int, sequence))
+    try:
+        i_seq = list(map(int, sequence))
 
-    share_of_units = sum(i_seq) / len(i_seq) 
-    if fabs(share_of_units-(1/2)) >= 2/sqrt(len(i_seq)):
-        return 0.0
-    
-    number_of_sign_changes = 0
-    for i in range(0, len(i_seq)-1):
-        number_of_sign_changes += 0 if i_seq[i] == i_seq[i+1] else 1
+        share_of_units = sum(i_seq) / len(i_seq) 
+        if fabs(share_of_units-(1/2)) >= 2/sqrt(len(i_seq)):
+            return 0.0
+        
+        number_of_sign_changes = 0
+        for i in range(0, len(i_seq)-1):
+            number_of_sign_changes += 0 if i_seq[i] == i_seq[i+1] else 1
 
-    p_value = erfc(
-        fabs(number_of_sign_changes-2*len(i_seq)*share_of_units*(1-share_of_units))
-        /(2*sqrt(2*len(i_seq))*share_of_units*(1-share_of_units))
-        )
-    
-    return p_value
+        p_value = erfc(
+            fabs(number_of_sign_changes-2*len(i_seq)*share_of_units*(1-share_of_units))
+            /(2*sqrt(2*len(i_seq))*share_of_units*(1-share_of_units))
+            )
+        
+        return p_value
+    except Exception as ex:
+        logging.error(f"Invalid sequence : {ex}")
 
 
 def longest_sequence_of_ones_in_the_block(sequence : str) -> float:
@@ -61,34 +67,37 @@ def longest_sequence_of_ones_in_the_block(sequence : str) -> float:
     
     Return: float - Probability of a random sequence
     """
-    i_seq = list(map(int, sequence))
-    block_size = 8
-    blocks = [i_seq[i:i + block_size] for i in range(0, len(i_seq), block_size)]
-    v_i = {1:0, 2:0, 3:0, 4:0}
-    for block in blocks:
-        max_once = 0
-        max_once_now = 0
-        for bit in block:
-            max_once_now = (max_once_now + 1) if bit == 1 else 0
-            if max_once < max_once_now:
-                max_once = max_once_now
-        match max_once:
-            case range(0, 2):
-                v_i[1] += 1
-            case 2:
-                v_i[2] += 1
-            case 3:
-                v_i[3] += 1
-            case range(4, 9):
-                v_i[4] += 1
-            case _:
-                pass
-    x_square = 0
-    for i, v in v_i.items():
-        x_square += (v-16*PI_I[i])**2 / 16*PI_I[i]
-    
-    p_value = gammainc(3/2, x_square/2)
-    return p_value
+    try:
+        i_seq = list(map(int, sequence))
+        block_size = 8
+        blocks = [i_seq[i:i + block_size] for i in range(0, len(i_seq), block_size)]
+        v_i = {1:0, 2:0, 3:0, 4:0}
+        for block in blocks:
+            max_once = 0
+            max_once_now = 0
+            for bit in block:
+                max_once_now = (max_once_now + 1) if bit == 1 else 0
+                if max_once < max_once_now:
+                    max_once = max_once_now
+            match max_once:
+                case range(0, 2):
+                    v_i[1] += 1
+                case 2:
+                    v_i[2] += 1
+                case 3:
+                    v_i[3] += 1
+                case range(4, 9):
+                    v_i[4] += 1
+                case _:
+                    pass
+        x_square = 0
+        for i, v in v_i.items():
+            x_square += (v-16*PI_I[i])**2 / 16*PI_I[i]
+        
+        p_value = gammainc(3/2, x_square/2)
+        return p_value
+    except Exception as ex:
+        logging.error(f"Invalid sequence : {ex}")
 
 
 if __name__ == "__main__":
