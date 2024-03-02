08a88dbd1f0036e99be8461f87e3a47f521076d5
Борис Шестериков <boris20033@gmail.com>
2024-03-02 20:10:04 +0400

    Make longest_sequence_of_ones_in_the_block test

--- lab_2/NIST_tests.py
+++ lab_2/NIST_tests.py
@@ -1,11 +1,14 @@
 import json
 import os
 import logging
+
 from math import fabs, sqrt, erfc
+from mpmath import gammainc
 
 logging.basicConfig(level=logging.INFO)
 
 SEQUENCE_PATH = os.path.join('lab_2', 'sequence.json')
+PI_I = {1:0.2148, 2:0.3672, 3:0.2305, 4:0.1875}
 
 def frequency_bit_test(sequence : str) -> float:
     i_seq = list(map(int, sequence))
@@ -34,7 +37,38 @@ def identical_consecutive_bits(sequence : str) -> float:
     return p_value
 
 
+def longest_sequence_of_ones_in_the_block(sequence : str) -> float:
+    i_seq = list(map(int, sequence))
+    block_size = 8
+    blocks = [i_seq[i:i + block_size] for i in range(0, len(i_seq), block_size)]
+    v_i = {1:0, 2:0, 3:0, 4:0}
+    for block in blocks:
+        max_once = 0
+        max_once_now = 0
+        for bit in block:
+            max_once_now = (max_once_now + 1) if bit == 1 else 0
+            if max_once < max_once_now:
+                max_once = max_once_now
+        match max_once:
+            case range(0, 2):
+                v_i[1] += 1
+            case 2:
+                v_i[2] += 1
+            case 3:
+                v_i[3] += 1
+            case range(4, 9):
+                v_i[4] += 1
+            case _:
+                pass
+    x_square = 0
+    for i, v in v_i.items():
+        x_square += (v-16*PI_I[i])**2 / 16*PI_I[i]
+    
+    p_value = gammainc(3/2, x_square/2)
+    return p_value
 
+
+    
 
 if __name__ == "__main__":
     with open(SEQUENCE_PATH, 'r', encoding='utf-8') as f:
@@ -45,3 +79,4 @@ if __name__ == "__main__":
 
     print(frequency_bit_test(cpp_seq))
     print(identical_consecutive_bits(cpp_seq))
+    print(longest_sequence_of_ones_in_the_block(java_seq))
