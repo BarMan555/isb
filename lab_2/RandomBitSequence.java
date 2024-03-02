f0e27a2573acec51d112671c9555d466c42f5a17
Борис Шестериков <boris20033@gmail.com>
2024-03-02 20:40:20 +0400

    Add docstring

--- lab_2/RandomBitSequence.java
+++ lab_2/RandomBitSequence.java
@@ -1,13 +1,22 @@
 import java.util.Random;
 
+/**
+ * A class to generate a random bit sequence of length 128.
+ */
 public class RandomBitSequence {
+
+     /**
+     * Main method to generate and print a random bit sequence of length SIZE.
+     *
+     * @param args The command-line arguments.
+     */
     public static void main(String[] args) {
         Random random = new Random();
         final int SIZE = 128;
 
         // Генерация 128 битовой последовательности из 0 и 1 с использованием массива boolean
         boolean[] randomBits = new boolean[SIZE];
-        for (int i = 0; i < 128; i++) {
+        for (int i = 0; i < SIZE; i++) {
             randomBits[i] = random.nextBoolean();
         }
 
--- lab_2/generator.cpp
+++ lab_2/generator.cpp
@@ -4,6 +4,9 @@
 
 #define SIZE 128
 
+/**
+ * A function to generate a random bit sequence of length SIZE.
+ */
 void generator()
 {
     // Генератор случайных чисел
@@ -12,13 +15,18 @@ void generator()
 
     // Генерация 128 битовой псевдослучайной последовательности
     std::array<bool, SIZE> randomBits;
-    for (int i = 0; i < 128; ++i) 
+    for (int i = 0; i < SIZE; ++i) 
     {
         randomBits[i] = gen() % 2;
         std::cout << randomBits[i];
     }
 }
 
+/**
+ * Main function to start the program.
+ *
+ * @return 0 on success
+ */
 int main() {
     generator();    
     return 0;
