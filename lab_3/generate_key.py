e95d7f79fa375452f4fd9789797831493aaddcce
Борис Шестериков <boris20033@gmail.com>
2024-03-22 11:18:51 +0400

    Add generate keys for (a)symmetrical algorithm

--- lab_3/generate_key.py
+++ lab_3/generate_key.py
@@ -1,6 +1,19 @@
 import os
 import json
-import cryptography
 
-def key_for_symmetric_algorithm():
-    pass
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
+def key_for_symmetric_algorithm() -> bytes:
+    key = os.urandom(32)
+    return key
+
+
+def keys_for_asymmetric_algorithm() -> tuple:
+    keys = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048
+    )
+    private_key = keys
+    public_key = keys.public_key()
+    return (public_key, private_key)
