eca6565f00c6d2e554403c67df08ee46864e4018
Борис Шестериков <boris20033@gmail.com>
2024-04-17 01:11:38 +0400

    Add open json file

--- lab_3/main.py
+++ lab_3/main.py
@@ -1,4 +1,9 @@
 import argparse
+import json
+
+from SymmetricKey import SymmetricKey
+from AsymmetricKey import AsymmetricalKey
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -6,8 +11,12 @@ if __name__ == "__main__":
     group.add_argument('-gen','--generation',help='Запускает режим генерации ключей')
     group.add_argument('-enc','--encryption',help='Запускает режим шифрования')
     group.add_argument('-dec','--decryption',help='Запускает режим дешифрования')
-
+    parser.add_argument('-s', '--settings',help="Файл с пользовательскими настройками")
     args = parser.parse_args()
+
+    with open(args.settings, 'r', encoding='utf-8') as file:
+        settings = json.load(file)
+
     if args.generation is not None:
         pass
     elif args.encryption is not None:
