059f7664a45808b956a8bcc3a2ae5701cf31ef8a
Борис Шестериков <boris20033@gmail.com>
2024-04-16 17:27:59 +0400

    Make write_data, serial/DEserial symmetric key

--- lab_3/WorkingWithFiles.py
+++ lab_3/WorkingWithFiles.py
@@ -4,10 +4,38 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
+
+def write_data(text : str, path : str) -> None:
+    try:
+        with open(path, 'w', encoding='utf-8') as file:
+            file.write(text)
+    except Exception as ex:
+        logging.error(ex)
+
+
+
 def read_data(path : str) -> str:
     try:
         with open(path, 'r', encoding='utf-8') as file:
             data = file.read()
         return data
+    except Exception as ex:
+        logging.error(ex)
+
+
+def serialization_symmetric_key(key : bytes, path : str) -> None:
+    try:
+        with open(path, 'wb') as key_file:
+            key_file.write(key)
+    except Exception as ex:
+        logging.error(ex)
+
+
+
+def deserialization_symmetric_key(path : str) -> bytes:
+    try:
+        with open(path, mode='rb') as key_file: 
+            content = key_file.read()
+        return content
     except Exception as ex:
         logging.error(ex)
