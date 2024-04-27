5797767ac67b0962d8e038cb4c431bee198f7223
Борис Шестериков <boris20033@gmail.com>
2024-04-27 13:42:44 +0400

    Delete if/elif. Add match/case

--- lab_3/main.py
+++ lab_3/main.py
@@ -8,10 +8,10 @@ from WorkingWithFiles import serialization_public_key, serialization_private_key
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    group = parser.add_mutually_exclusive_group(required = True)
-    group.add_argument('-gen','--generation', action='store_true', help='Запускает режим генерации ключей')
-    group.add_argument('-enc','--encryption', action='store_true', help='Запускает режим шифрования')
-    group.add_argument('-dec','--decryption', action='store_true', help='Запускает режим дешифрования')
+    parser.add_argument('-m', '--mode', type=int, help='Выбор режима работы: '
+                             '0 - Запускает режим генерации ключей'
+                             '1 - Запускает режим шифрования'
+                             '2 - Запускает режим дешифрования') 
     parser.add_argument('-s', '--settings',help="Файл с пользовательскими настройками")
     args = parser.parse_args()
 
@@ -21,25 +21,26 @@ if __name__ == "__main__":
     symmetric = SymmetricKey()
     asymmetric = AsymmetricalKey()
 
-    if args.generation:
-        symmetric_key = symmetric.generation_symm_key()
-        public_key, private_key = asymmetric.generation_asym_keys()
-        serialization_public_key(public_key, settings['public_key'])
-        serialization_private_key(private_key, settings['private_key'])
-        asymmetric.encrypt_symm_key(settings['symmetric_key'],
-                                    settings['public_key'],
-                                    settings['encrypted_symmetric_key'])
-    elif args.encryption:
-        print(asymmetric.decrypt_symm_key(settings['encrypted_symmetric_key'],
-                                    settings['private_key'],
-                                    settings['decrypted_symmetric_key']))
-        symmetric.encrypt_text(settings['initial_file'],
-                               settings['encrypted_file'],
-                               settings['decrypted_symmetric_key'])
-    else:
-        asymmetric.encrypt_symm_key(settings['symmetric_key'],
-                                    settings['public_key'],
-                                    settings['encrypted_symmetric_key'])
-        symmetric.decrypt_text(settings['encrypted_file'],
-                               settings['decrypted_file'],
-                               settings['encrypted_symmetric_key'])
+    match args.mode:
+        case 0:
+            symmetric_key = symmetric.generation_symm_key()
+            public_key, private_key = asymmetric.generation_asym_keys()
+            serialization_public_key(public_key, settings['public_key'])
+            serialization_private_key(private_key, settings['private_key'])
+            asymmetric.encrypt_symm_key(settings['symmetric_key'],
+                                        settings['public_key'],
+                                        settings['encrypted_symmetric_key'])
+        case 1:
+            print(asymmetric.decrypt_symm_key(settings['encrypted_symmetric_key'],
+                                        settings['private_key'],
+                                        settings['decrypted_symmetric_key']))
+            symmetric.encrypt_text(settings['initial_file'],
+                                settings['encrypted_file'],
+                                settings['decrypted_symmetric_key'])
+        case 2:
+            asymmetric.encrypt_symm_key(settings['symmetric_key'],
+                                        settings['public_key'],
+                                        settings['encrypted_symmetric_key'])
+            symmetric.decrypt_text(settings['encrypted_file'],
+                                settings['decrypted_file'],
+                                settings['encrypted_symmetric_key'])
