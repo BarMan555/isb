f0370dee61a7bf71e17957bf914e5382bb5be126
Борис Шестериков <boris20033@gmail.com>
2024-05-04 15:14:08 +0400

    Add analysis function

--- lab_4/functions_for_card.py
+++ lab_4/functions_for_card.py
@@ -2,6 +2,8 @@ import logging
 import multiprocessing as mp
 import hashlib
 import json
+import time
+from matplotlib import pyplot as plt
 
 logging.basicConfig(level=logging.INFO)
 
@@ -34,18 +36,37 @@ def number_search(save_path : str, hash : str, last_digits : str, bins : list) -
 def luna_algorithm(card_number : str) -> bool:
     """
     """
-    check_digit = int(card_number[-1])
-    card_number_list = [int(card_number[-i]) for i in range(1, len(card_number))]
-    for i in card_number_list:
-        card_number_list[i] *= 2
-        if card_number_list[i] >= 10:
-            card_number_list[i] = (card_number[i] % 10) + (card_number[i] // 10)
+    try:
+        check_digit = int(card_number[-1])
+        card_number_list = [int(card_number[-i]) for i in range(2, len(card_number))]
+        for i in range(len(card_number_list)):
+            if(i%2==0):
+                card_number_list[i] *= 2
+                if card_number_list[i] >= 10:
+                    card_number_list[i] = (card_number_list[i] % 10) + (card_number_list[i] // 10)
 
-    sum = sum(card_number_list)
-    check_sum = (10 - (sum % 10)) % 10
-    return check_sum == check_digit
+        sum_ditits = sum(card_number_list)
+        check_sum = (10 - ((sum_ditits % 10)%10))
+        return check_sum == check_digit
+    except Exception as ex:
+        logging.error(ex)
 
 
-        
+def analysis_time_search_hash_collision(hash : str, last_digits : str, bins : list):
+    """
+    """
+    times = list()
+    for i in range(1, int(mp.cpu_count()*1.5)):
+        start = time.time()
+        with mp.Pool(i) as p:
+            for result in p.starmap(check_number_card, [(i, hash, last_digits, bins) for i in range(0, 999999)]):
+                if result is not None:
+                    times.append(time.time() - start)
+                    break
+    plt.plot(range(len(times)), times, color="green", marker="o", markersize=7,)
+    plt.xlabel("Количество процессов, Шт")
+    plt.ylabel("Время, С")
+    plt.title("График зависимости времени поиска коллизии от числа процессов")
+    plt.show()
     
 
